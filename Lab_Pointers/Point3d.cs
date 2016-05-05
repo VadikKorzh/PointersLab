3f584466467fae50a7747b4a984eefa4837bd735
VadikKorzh <Kharkiv@brainacad.com>
2016-05-05 21:27:55 +0300

    Unsafe mode lab work

---
+++ Lab_Pointers/Point3d.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Pointers
+{
+    struct Point3d
+    {
+        public int X;
+        public int Y;
+        public int Z;
+
+        public override string ToString()
+        {
+            return $"[{X}, {Y}, {Z}]";
+        }
+    }
+}
---
+++ Lab_Pointers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Pointers
+{
+    class Program
+    {
+        unsafe static void ShowInfo(Point3d* p3dPtr)
+        {
+            Point3d p = *p3dPtr;
+            Console.WriteLine($"Size of {p.GetType().Name}: {sizeof(Point3d)}");
+            Console.WriteLine($"{nameof(p.X)} = {p3dPtr->X}");
+            Console.WriteLine($"{nameof(p.Y)} = {p3dPtr->Y}");
+            Console.WriteLine($"{nameof(p.Z)} = {p3dPtr->Z}");
+        }
+
+        unsafe static void Main(string[] args)
+        {
+            Point3d p1 = new Point3d { X = 222, Y = 111, Z = 333 };
+            Console.WriteLine("Initialized point: " + p1);
+
+            Point3d* p3dPtr = &p1;
+            p3dPtr->X = 0;
+            p3dPtr->Y = 0;
+            p3dPtr->Z = -1111;
+
+            Console.WriteLine("Modified via pointers point: " + p1);
+            ShowInfo(p3dPtr);
+        }
+    }
+}
